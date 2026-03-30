4eb4227cd09915083de366529ddefdb4f4fe3245
elwrcl <elwerici@icloud.com>
2026-03-31 01:18:23 +0300

    fix: zapret firewall rules

--- nixos/networking/network-manager/default.nix
+++ nixos/networking/network-manager/default.nix
@@ -4,15 +4,16 @@
   networking.hostName = "copland";
   networking.networkmanager.enable = true;
 
-services.resolved = {
-  enable = true;
-  dnssec = "false";
-  domains = [ "~." ];
-  fallbackDns = [ "1.1.1.1" "8.8.8.8" ];
-  settings = {
-    Resolve = {
-      DNS = "1.1.1.1";
-      DNSOverTLS = "yes";
+  services.resolved = {
+    enable = true;
+    dnssec = "false";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1" "8.8.8.8" ];
+    settings = {
+      Resolve = {
+        DNS = "1.1.1.1";
+        DNSOverTLS = "yes";
+      };
     };
   };
-};
+}
