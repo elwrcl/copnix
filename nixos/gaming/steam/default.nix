93575de9a82e8872f85faf23cd22519767fbb5e6
elwrcl <elwerici@icloud.com>
2026-04-06 20:33:01 +0300

    add: extra steam fixes

--- nixos/gaming/steam/default.nix
+++ nixos/gaming/steam/default.nix
@@ -1,5 +1,34 @@
 { config, pkgs, ... }:
 
 {
-  programs.steam.enable = true;
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+    package = pkgs.steam.override {
+      extraPkgs = p: with p; [
+        liberation_ttf
+        wqy_zenhei
+        corefonts
+        vistafonts
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+        libgpg-error
+        libkrb5
+        keyutils
+        bash
+        coreutils
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib
+        gamemode
+      ];
+    };
+    gamescopeSession.enable = true;
+  };
+  programs.bash.enable = true;
 }
