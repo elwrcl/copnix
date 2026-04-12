d9dc88150441da627c0a6a9e0f94b3ffab78d6aa
elwrcl <elwerici@icloud.com>
2026-04-12 21:33:32 +0300

    update

--- flake.nix
+++ flake.nix
@@ -1,67 +1,24 @@
-
 {
   description = "copland nixos configurations";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-
     nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
-
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
-
     copetch.url = "github:elwrcl/copetch";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     helium = {
       url = "github:AlvaroParker/helium-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     apple-fonts = {
       url = "github:Lyndeno/apple-fonts.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-  {
-  description = "copland nixos configurations";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-
-    nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
-
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
-
-    copetch.url = "github:elwrcl/copetch";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    helium = {
-      url = "github:AlvaroParker/helium-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    apple-fonts = {
-      url = "github:Lyndeno/apple-fonts.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -101,12 +58,10 @@
           modules = [
             chaotic.nixosModules.default
             home-manager.nixosModules.home-manager
-
             {
-              nixpkgs.overlays = [ chaoticOverlay cachyosKernelOverlay ];
+              nixpkgs.overlays = [ chaoticOverlay cachyosKernelOverlay gtkPortalOverlay ];
               nixpkgs.config.allowUnfree = true;
             }
-
             {
               home-manager = {
                 useGlobalPkgs = true;
@@ -115,60 +70,10 @@
                 users.elars = import ./home;
               };
             }
-
             ./machine.nix
             ./nixos/default.nix
           ];
         };
       };
     };
-}
-
-
-  outputs = inputs@{ self, nixpkgs, chaotic, home-manager, nix-cachyos-kernel, ... }:
-let
-system = "x86_64-linux";
-
-chaoticOverlay = chaotic.overlays.default;
-cachyosKernelOverlay = nix-cachyos-kernel.overlays.pinned;
-
-copetchOverlay = final: prev: {
-copetch = inputs.copetch.packages.${system}.default;
-};
-
-in
-{
-nixosConfigurations = {
-copland = nixpkgs.lib.nixosSystem {
-system = system;
-specialArgs = {
-inherit inputs system;
-zen-browser = inputs.zen-browser;
-nix-cachyos-kernel = nix-cachyos-kernel;
-};
-
-modules = [
-chaotic.nixosModules.default
-home-manager.nixosModules.home-manager
-
-{
-nixpkgs.overlays = [ chaoticOverlay cachyosKernelOverlay ];
-nixpkgs.config.allowUnfree = true;
-}
-
-{
-home-manager = {
-useGlobalPkgs = true;
-useUserPackages = true;
-extraSpecialArgs = { inherit inputs system; };
-users.elars = import ./home;
-};
-}
-
-./machine.nix
-./nixos/default.nix
-];
-};
-};
-};
 }
