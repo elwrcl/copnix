1c1ba6b242d34e090c8d1cd011e29cebf2886423
elwrcl <elwerici@icloud.com>
2026-04-18 01:17:27 +0300

    feat(security): enable GNOME Keyring service

--- nixos/services/security/default.nix
+++ nixos/services/security/default.nix
@@ -2,10 +2,10 @@
 
 {
   programs.gnupg.agent = {
-     enable = true;
-     enableSSHSupport = false;
+    enable = true;
+    enableSSHSupport = false;
   };
-
+  services.gnome.gnome-keyring.enable = true;
   security.wrappers.gsr-kms-server = {
     owner = "root";
     group = "root";
