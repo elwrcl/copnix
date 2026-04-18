de1f998a87235a8004cc1598ef1b71d89020a4ea
elwrcl <elwerici@icloud.com>
2026-04-19 00:59:58 +0300

    changed zram mem.percent
    Signed-off-by: elwrcl <elwerici@icloud.com>

--- machine.nix
+++ machine.nix
@@ -17,18 +17,16 @@ in
 
   zramSwap = {
     enable = true;
-    memoryPercent = 100;
+    memoryPercent = 25;
     algorithm = "zstd";
     priority = 100;
   };
-
   environment.systemPackages = packages.system ++ [
     inputs.helium.packages.${system}.default
     inputs.zen-browser.packages.${system}.default
     inputs.noctalia.packages.${system}.default
     inputs.copetch.packages.${system}.default
   ];
-
   environment.pathsToLink = [
     "/share/applications"
     "/share/icons"
