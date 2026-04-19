830ca94bbd96c27e0ef4344f87d7cfc338b3ff16
elwrcl <elwerici@icloud.com>
2026-04-19 19:19:12 +0300

    portal fix
    Signed-off-by: elwrcl <elwerici@icloud.com>

--- nixos/desktop/window-managers/niri.nix
+++ nixos/desktop/window-managers/niri.nix
@@ -4,14 +4,25 @@
   programs.niri.enable = true;
 
   xdg.portal = {
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-wlr
+    ];
+
     config.niri = lib.mkForce {
       default = [
-        "gnome"
+        "wlr"
         "gtk"
       ];
-      "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
-      "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
+      "org.freedesktop.impl.portal.ScreenCast" = [ "wlr" ];
+      "org.freedesktop.impl.portal.Screenshot" = [ "wlr" ];
       "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
     };
   };
+  environment.etc."xdg-desktop-portal/portals/wlr.portal".text = ''
+    [portal]
+    DBusName=org.freedesktop.impl.portal.desktop.wlr
+    Interfaces=org.freedesktop.impl.portal.Screenshot;org.freedesktop.impl.portal.ScreenCast;
+    UseIn=wlroots;sway;Wayfire;river;phosh;Hyprland;niri;
+  '';
 }
