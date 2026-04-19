608a467d84a3f3b07b59436da68ccaf965f99199
elwrcl <elwerici@icloud.com>
2026-04-19 19:23:04 +0300

    fix
    Signed-off-by: elwrcl <elwerici@icloud.com>

--- nixos/desktop/window-managers/niri.nix
+++ nixos/desktop/window-managers/niri.nix
@@ -5,24 +5,18 @@
 
   xdg.portal = {
     extraPortals = [
+      pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-wlr
     ];
 
     config.niri = lib.mkForce {
       default = [
-        "wlr"
+        "hyprland"
         "gtk"
       ];
-      "org.freedesktop.impl.portal.ScreenCast" = [ "wlr" ];
-      "org.freedesktop.impl.portal.Screenshot" = [ "wlr" ];
+      "org.freedesktop.impl.portal.ScreenCast" = [ "hyprland" ];
+      "org.freedesktop.impl.portal.Screenshot" = [ "hyprland" ];
       "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
     };
   };
-  environment.etc."xdg-desktop-portal/portals/wlr.portal".text = ''
-    [portal]
-    DBusName=org.freedesktop.impl.portal.desktop.wlr
-    Interfaces=org.freedesktop.impl.portal.Screenshot;org.freedesktop.impl.portal.ScreenCast;
-    UseIn=wlroots;sway;Wayfire;river;phosh;Hyprland;niri;
-  '';
 }
