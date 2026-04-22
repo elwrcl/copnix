4d8b7c5bc8e6dd731c2b0b4a90adfdb912c010a5
elwrcl <elwerici@icloud.com>
2026-04-22 19:27:31 +0300

    updated hyprland.nix for xdg.portal
    Signed-off-by: elwrcl <elwerici@icloud.com>

--- nixos/desktop/window-managers/hyprland.nix
+++ nixos/desktop/window-managers/hyprland.nix
@@ -16,8 +16,19 @@
 
   xdg.portal = {
     enable = true;
-    extraPortals = [ inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland ];
-    config.common.default = "*";
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config = {
+      common.default = "*";
+      hyprland = {
+        default = [
+          "hyprland"
+          "gtk"
+        ];
+        "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+        "org.freedesktop.impl.portal.Screenshot" = [ "hyprland" ];
+        "org.freedesktop.impl.portal.ScreenCast" = [ "hyprland" ];
+      };
+    };
   };
 
   services = {
