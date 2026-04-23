6a8206d306c5c014483de6d6c3c942ad63dd4795
elwrcl <elwerici@icloud.com>
2026-04-23 10:58:11 +0300

    update kmscon config

--- nixos/system/kmscon/default.nix
+++ nixos/system/kmscon/default.nix
@@ -2,12 +2,12 @@
 {
   services.kmscon = {
     enable = true;
-    fonts = [
-      {
-        name = "JetBrainsMono Nerd Font";
-        package = pkgs.nerd-fonts.jetbrains-mono;
-      }
-    ];
+    autologin = true;
+    hwRender = true;
+    fonts = [{
+      name = "JetBrainsMono Nerd Font";
+      package = pkgs.nerd-fonts.jetbrains-mono;
+    }];
     extraConfig = ''
       font-size=12
     '';
