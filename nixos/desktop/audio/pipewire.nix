edceefd595ae4ee0bae7b994fc6e161cbf76343c
elwrcl <elwerici@icloud.com>
2026-05-18 20:54:35 +0300

    Update pipewire.nix

--- nixos/desktop/audio/pipewire.nix
+++ nixos/desktop/audio/pipewire.nix
@@ -3,35 +3,35 @@
 {
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
-
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-
-    wireplumber = {
-      enable = true;
-      extraConfig."51-no-mic-monitor" = {
-        "monitor.alsa.rules" = [
-          {
-            matches = [ { "node.name" = "~alsa_input.*"; } ];
-            actions = {
-              "update-props" = {
-                "stream.dont-remix" = true;
-              };
-            };
-          }
-        ];
-      };
+    wireplumber.enable = true;
+    extraConfig.pipewire."10-realtime" = {
+      "context.modules" = [
+        {
+          name = "libpipewire-module-rt";
+          args = {
+            "nice.level" = -15;
+            "rt.prio" = 88;
+            "rt.time.soft" = 200000;
+            "rt.time.hard" = 400000;
+          };
+          flags = [
+            "ifexists"
+            "nofail"
+          ];
+        }
+      ];
     };
-
     extraConfig.pipewire."92-quantum" = {
       "context.properties" = {
         "default.clock.rate" = 48000;
         "default.clock.allowed-rates" = [ 48000 ];
-        "default.clock.quantum" = 768;
-        "default.clock.min-quantum" = 512;
+        "default.clock.quantum" = 1024;
+        "default.clock.min-quantum" = 1024;
         "default.clock.max-quantum" = 1024;
       };
     };
