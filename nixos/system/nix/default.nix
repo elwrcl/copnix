bbd44fef919fd656dc71e55a5ec3d1c5b76e11f5
elwrcl <elwerici@icloud.com>
2026-05-23 10:45:58 +0300

    new substituters

--- nixos/system/nix/default.nix
+++ nixos/system/nix/default.nix
@@ -12,6 +12,8 @@
 
     substituters = [
       "https://cache.nixos.org"
+      "https://nix-gaming.cachix.org"
+      "https://ezkea.cachix.org"
       "https://nix-community.cachix.org"
       "https://noctalia.cachix.org"
       "https://attic.xuyh0120.win/lantian"
@@ -20,6 +22,8 @@
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Oj21/SB80OU="
+      "ezkea.cachix.org-1:ioBmUbk1F3Sn3KdlSaJZF6ErF7FuBBzfFO5t7SSSYSE="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
