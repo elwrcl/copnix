2e3a40da31e1e57cf73e891f2d5c4229cb0f85d4
elwrcl <elwerici@icloud.com>
2026-05-25 15:22:48 +0300

    test

--- nixos/system/nix/default.nix
+++ nixos/system/nix/default.nix
@@ -12,16 +12,16 @@
 
     substituters = [
       "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
       #"https://nix-gaming.cachix.org"
       #"https://ezkea.cachix.org"
-      #"https://nix-community.cachix.org"
       #"https://noctalia.cachix.org"
       #"https://hyprland.cachix.org"
       #"https://cache.garnix.io"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      #"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       #"nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       #"ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
       #"noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
