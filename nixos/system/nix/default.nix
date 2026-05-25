0bd7d0cdcbfa084fcc92bb711868c1b15ad9a6e3
elwrcl <elwerici@icloud.com>
2026-05-25 15:11:37 +0300

    testing

--- nixos/system/nix/default.nix
+++ nixos/system/nix/default.nix
@@ -12,23 +12,23 @@
 
     substituters = [
       "https://cache.nixos.org"
-      "https://nix-gaming.cachix.org"
-      "https://ezkea.cachix.org"
-      "https://nix-community.cachix.org"
-      "https://noctalia.cachix.org"
-      "https://hyprland.cachix.org"
-      "https://cache.garnix.io"
+      #"https://nix-gaming.cachix.org"
+      #"https://ezkea.cachix.org"
+      #"https://nix-community.cachix.org"
+      #"https://noctalia.cachix.org"
+      #"https://hyprland.cachix.org"
+      #"https://cache.garnix.io"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Oj21/SB80OU="
-      "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
-      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      #"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      #"nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      #"ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
+      #"noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+      #"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      #"cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
+
   programs.nix-ld.enable = true;
-  chaotic.nyx.cache.enable = false;
 }
