cc44d6a1ebd4c66386fcf3c4dc34bbfd5a6fa6ae
elwrcl <elwerici@icloud.com>
2026-05-30 16:27:53 +0300

    discord fix

--- modules/nixos/overlays/default.nix
+++ modules/nixos/overlays/default.nix
@@ -4,7 +4,8 @@
   nixpkgs.overlays = [
     (_: prev: {
       legcord = prev.legcord.overrideAttrs (old: {
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.nodePackages.asar ];
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.asar ];
+
         postInstall = (old.postInstall or "") + ''
           asarFile=$(find $out -name "app.asar" -print -quit)
           asar extract "$asarFile" "$TMPDIR/legcord-src"
