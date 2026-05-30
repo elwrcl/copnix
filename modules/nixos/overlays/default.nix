f1eae4a18ccc8d385cb87dfa749acb9e27b72ef3
elwrcl <elwerici@icloud.com>
2026-05-30 15:28:25 +0300

    overlay

---
+++ modules/nixos/overlays/default.nix
@@ -0,0 +1,18 @@
+{ ... }:
+
+{
+  nixpkgs.overlays = [
+    (_: prev: {
+      legcord = prev.legcord.overrideAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.nodePackages.asar ];
+        postInstall = (old.postInstall or "") + ''
+          asarFile=$(find $out -name "app.asar" -print -quit)
+          asar extract "$asarFile" "$TMPDIR/legcord-src"
+          substituteInPlace "$TMPDIR/legcord-src/ts-out/main.js" \
+            --replace 'corsEnabled: false,' 'corsEnabled: true,'
+          asar pack "$TMPDIR/legcord-src" "$asarFile"
+        '';
+      });
+    })
+  ];
+}
