9c2fd108534ee9796f68a1ea5afc32659a95b59d
elwrcl <elwerici@icloud.com>
2026-05-31 15:25:58 +0300

    removed scheduler

--- modules/nixos/services/performance/default.nix
+++ modules/nixos/services/performance/default.nix
@@ -1,10 +1,6 @@
 { ... }:
 
 {
-  services.scx = {
-    enable = false;
-    scheduler = "scx_rustland";
-  };
   services.irqbalance.enable = true;
   services.power-profiles-daemon.enable = true;
   services.upower.enable = true;
