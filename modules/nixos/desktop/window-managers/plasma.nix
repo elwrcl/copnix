e92d55df70d2009c480e7c82bda93a1f34f0d134
elwrcl <elwerici@icloud.com>
2026-06-08 20:58:38 +0300

    switch to plasma

---
+++ modules/nixos/desktop/window-managers/plasma.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+{
+  services.desktopManager.plasma6.enable = true;
+
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-kde ];
+  xdg.portal.config.plasma.default = [ "kde" ];
+
+  environment.systemPackages = with pkgs; [
+    kdePackages.plasma-browser-integration
+    kdePackages.kwalletmanager
+  ];
+}
