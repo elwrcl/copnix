acc07a3f9850168428ea19e3a5763d814289053f
elwrcl <elwerici@icloud.com>
2026-07-19 19:47:11 +0300

    my kernel

--- modules/nixos/system/nix/default.nix
+++ modules/nixos/system/nix/default.nix
@@ -17,6 +17,7 @@
       "https://noctalia.cachix.org"
       "https://hyprland.cachix.org"
       "https://attic.xuyh0120.win/lantian"
+      "https://soryu-kernel.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
@@ -25,6 +26,7 @@
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+      "soryu-kernel.cachix.org-1:2/Tm90ibGjLQWo/uX4qo/kbFB5yjQG/CzfE1oelHdWw="
     ];
   };
 
