2f9e84f7732ba90466a7645016147b8443c6d4e1
elwrcl <elwerici@icloud.com>
2026-07-23 23:01:40 +0300

    duzen tertip

--- modules/home/niri/niri/env.nix
+++ modules/home/niri/niri/env.nix
@@ -4,15 +4,15 @@
   programs.niri.settings = {
     environment = {
       DISPLAY = ":0";
-      XCURSOR_THEME = "macOS";
       XCURSOR_SIZE = "24";
-      GDK_BACKEND = "wayland,x11";
-      QT_QPA_PLATFORM = "wayland;xcb";
-      QT_QPA_PLATFORMTHEME = "kde";
-      ELECTRON_OZONE_PLATFORM_HINT = "auto";
       TERMINAL = "ghostty";
+      XCURSOR_THEME = "macOS";
+      GDK_BACKEND = "wayland,x11";
+      QT_QPA_PLATFORMTHEME = "gtk";
+      QT_QPA_PLATFORM = "wayland;xcb";
       MESA_GL_VERSION_OVERRIDE = "4.6";
       MESA_GLSL_VERSION_OVERRIDE = "460";
+      ELECTRON_OZONE_PLATFORM_HINT = "auto";
     };
   };
 }
