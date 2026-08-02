f50d02423126929e47017dd4ad954bec6291ade3
elwrcl <elwerici@icloud.com>
2026-08-02 04:19:52 +0300

    ug

--- modules/home/home-radicle.ulu.nix
+++ modules/home/home-radicle.ulu.nix
@@ -13,17 +13,18 @@
     in
     {
       home.packages = [ pkgs.radicle-node ];
-      home.file = {
-        ".radicle/config.json".text = builtins.toJSON {
-          publicExplorer = "https://radicle.network/nodes/$host/$rid$path";
-          preferredSeeds = [
-            "z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@iris.radicle.network:8776"
-            "z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@rosa.radicle.network:8776"
-          ];
-        };
-      }
-      // optionalAttrs hasSecret {
-        ".radicle/keys/radicle".source = osConfig.age.secrets.radicle-key.path;
+      home.file.".radicle/config.json".text = builtins.toJSON {
+        publicExplorer = "https://radicle.network/nodes/$host/$rid$path";
+        preferredSeeds = [
+          "z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@iris.radicle.network:8776"
+          "z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@rosa.radicle.network:8776"
+        ];
+      };
+      home.activation = optionalAttrs hasSecret {
+        radicleKey = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          run mkdir -p "$HOME/.radicle/keys"
+          run ln -sf "${osConfig.age.secrets.radicle-key.path}" "$HOME/.radicle/keys/radicle"
+        '';
       };
     };
 }
