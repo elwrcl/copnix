2649ebe6d43f52c544e155295cbdb5fb5b762df8
elwrcl <elwerici@proton.me>
2026-08-24 02:21:47 +0300

    hooho

--- modules/nixos/hw/hw-intel-hard.ulu.nix
+++ modules/nixos/hw/hw-intel-hard.ulu.nix
@@ -4,9 +4,6 @@
     { ... }:
     {
       imports = [ inputs.intel-hard.nixosModules.default ];
-
-      # No paths here on purpose: every one of them comes from the flake's
-      # topology.nix. If a tree moves, that file is the only edit.
       programs.intel-hard.enable = true;
     };
 }
